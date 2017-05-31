f904a46318be789a38f669e4984985864dbb9219
Jamie Townsend <jamiehntownsend@gmail.com>
2017-05-31 19:00:05 +0100

    Add hessian_vector_product

--- autograd_forward/__init__.py
+++ autograd_forward/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 
-from autograd_forward.convenience_wrappers import (jacobian_vector_product, #hessian_vector_product
-                                   forward_derivative)
+from autograd_forward.convenience_wrappers import (jacobian_vector_product,
+                                                   hessian_vector_product,
+                                                   forward_derivative)
 import autograd_forward.numpy
 import autograd_forward.scipy
 import autograd_forward.container_types
--- autograd_forward/convenience_wrappers.py
+++ autograd_forward/convenience_wrappers.py
@@ -2,8 +2,8 @@
 from __future__ import absolute_import
 
 from autograd.convenience_wrappers import (attach_name_and_doc, safe_type,
-                                           cast_to_same_dtype)
-
+                                           cast_to_same_dtype, grad)
+from autograd.convenience_wrappers import hessian_vector_product as ahvp
 from autograd_forward.core import make_jvp
 
 
@@ -53,25 +53,21 @@ def forward_derivative(fun, argnum=0):
 #
 #     return jacfun
 
-# def hessian_vector_product(fun, argnum=0, method='rev-rev'):
-#     """Builds a function that returns the exact Hessian-vector product.
-#     The returned function has arguments (*args, vector, **kwargs), and takes
-#     roughly 4x as long to evaluate as the original function.
-#
-#     There are two methods available, specified by the `method' parameter:
-#     rev-rev (default) and fwd-rev. fwd-rev is faster and has lower memory
-#     overhead but is incompatible with some primitives."""
-#     if method == 'rev-rev':
-#         fun_grad = grad(fun, argnum)
-#         def vector_dot_grad(*args, **kwargs):
-#             args, vector = args[:-1], args[-1]
-#             return np.tensordot(fun_grad(*args, **kwargs), vector, np.ndim(vector))
-#         return grad(vector_dot_grad, argnum)  # Grad wrt original input.
-#     elif method == 'fwd-rev':
-#         return jacobian_vector_product(grad(fun, argnum), argnum)
-#     else:
-#         raise ValueError("{} is not a valid method for hessian_vector_product. "
-#                          "Valid methods are: 'rev-rev', 'fwd-rev'.".format(method))
+def hessian_vector_product(fun, argnum=0, method='rev-rev'):
+    """Builds a function that returns the exact Hessian-vector product.
+    The returned function has arguments (*args, vector, **kwargs), and takes
+    roughly 4x as long to evaluate as the original function.
+
+    There are two methods available, specified by the `method' parameter:
+    rev-rev (default) and fwd-rev. fwd-rev is faster and has lower memory
+    overhead but is incompatible with some primitives."""
+    if method == 'rev-rev':
+        return ahvp(fun, argnum)
+    elif method == 'fwd-rev':
+        return jacobian_vector_product(grad(fun, argnum), argnum)
+    else:
+        raise ValueError("{} is not a valid method for hessian_vector_product. "
+                         "Valid methods are: 'rev-rev', 'fwd-rev'.".format(method))
 
 def jacobian_vector_product(fun, argnum=0):
     """Builds a function that returns the exact Jacobian-vector product, that

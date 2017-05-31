f9901b206e86c994bc10dec24573679096ba9a30
Jamie Townsend <jamiehntownsend@gmail.com>
2017-05-31 19:03:21 +0100

    Remove jacobian cruft from conv wrappers

--- autograd_forward/convenience_wrappers.py
+++ autograd_forward/convenience_wrappers.py
@@ -20,39 +20,6 @@ def forward_derivative(fun, argnum=0):
         return d
     return dervfun
 
-# def jacobian(fun, argnum=0):
-#     """
-#     Returns a function which computes the Jacobian of `fun` with respect to
-#     positional argument number `argnum`, which must be a scalar or array. Unlike
-#     `grad` it is not restricted to scalar-output functions, but also it cannot
-#     take derivatives with respect to some argument types (like lists or dicts).
-#     If the input to `fun` has shape (in1, in2, ...) and the output has shape
-#     (out1, out2, ...) then the Jacobian has shape (out1, out2, ..., in1, in2, ...).
-#     """
-#     def getshape(val):
-#         val = getval(val)
-#         assert np.isscalar(val) or isinstance(val, np.ndarray), \
-#             'Jacobian requires input and output to be scalar- or array-valued'
-#         return np.shape(val)
-#
-#     def unit_vectors(shape):
-#         for idxs in it.product(*map(range, shape)):
-#             vect = np.zeros(shape)
-#             vect[idxs] = 1
-#             yield vect
-#
-#     concatenate = lambda lst: np.concatenate(map(np.atleast_1d, lst))
-#
-#     @attach_name_and_doc(fun, argnum, 'Jacobian')
-#     def jacfun(*args, **kwargs):
-#         vjp, ans = make_vjp(fun, argnum)(*args, **kwargs)
-#         outshape = getshape(ans)
-#         grads = map(vjp, unit_vectors(outshape))
-#         jacobian_shape = outshape + getshape(args[argnum])
-#         return np.reshape(concatenate(grads), jacobian_shape)
-#
-#     return jacfun
-
 def hessian_vector_product(fun, argnum=0, method='rev-rev'):
     """Builds a function that returns the exact Hessian-vector product.
     The returned function has arguments (*args, vector, **kwargs), and takes

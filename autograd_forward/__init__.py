202475dc29313197fbcea31d32197b45ae03835a
Jamie Townsend <jamiehntownsend@gmail.com>
2017-05-31 13:01:13 +0100

    Add numpy __init__

--- autograd_forward/__init__.py
+++ autograd_forward/__init__.py
@@ -2,8 +2,7 @@ from __future__ import absolute_import
 
 from autograd_forward.convenience_wrappers import (jacobian_vector_product, #hessian_vector_product
                                    forward_derivative)
-import autograd_forward.numpy.numpy_grads
-import autograd_forward.numpy.numpy_wrapper
-import autograd_forward.numpy.numpy_extra
+import autograd_forward.numpy
+import autograd_forward.scipy
 import autograd_forward.container_types
 import autograd_forward.core
---
+++ autograd_forward/numpy/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from . import numpy_extra
+from . import numpy_grads
+from . import numpy_wrapper

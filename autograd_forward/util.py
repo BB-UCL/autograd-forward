bb200358be641df41c3ae479bfca422f4a1a1709
Jamie Townsend <jamiehntownsend@gmail.com>
2017-06-01 14:02:43 +0100

    flatten compatibility with autograd master

--- autograd_forward/util.py
+++ autograd_forward/util.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import
 
 from builtins import range
+from operator import itemgetter
+from future.utils import iteritems
 
-from autograd.core import vspace, getval
+from autograd.core import getval
+import autograd.numpy as np
+from autograd.container_types import make_dict, make_tuple, make_list
 import autograd.util as au
 from autograd_forward.convenience_wrappers import forward_derivative
 
@@ -32,8 +36,39 @@ def flatten(value):
        Returns 1D numpy array and an unflatten function.
        Doesn't preserve mixed numeric types (e.g. floats and ints).
        Assumes dict keys are sortable."""
-    try:
-        vs = vspace(getval(value))
-    except TypeError:
+    if isinstance(getval(value), np.ndarray):
+        shape = value.shape
+        def unflatten(vector):
+            return np.reshape(vector, shape)
+        return np.ravel(value), unflatten
+
+    elif isinstance(getval(value), (float, int, complex)):
+        return np.array([value]), lambda x : x[0]
+
+    elif isinstance(getval(value), (tuple, list)):
+        constructor = make_tuple if isinstance(getval(value), tuple) else make_list
+        if not value:
+            return np.array([]), lambda x : constructor()
+        flat_pieces, unflatteners = zip(*map(flatten, value))
+        split_indices = np.cumsum([len(vec) for vec in flat_pieces[:-1]])
+
+        def unflatten(vector):
+            pieces = np.split(vector, split_indices)
+            return constructor(*[unflatten(v) for unflatten, v in zip(unflatteners, pieces)])
+
+        return np.concatenate(flat_pieces), unflatten
+
+    elif isinstance(getval(value), dict):
+        items = sorted(iteritems(value), key=itemgetter(0))
+        keys, flat_pieces, unflatteners = zip(*[(k,) + flatten(v) for k, v in items])
+        split_indices = np.cumsum([len(vec) for vec in flat_pieces[:-1]])
+
+        def unflatten(vector):
+            pieces = np.split(vector, split_indices)
+            return make_dict([(key, unflattener(piece))
+                             for piece, unflattener, key in zip(pieces, unflatteners, keys)])
+
+        return np.concatenate(flat_pieces), unflatten
+
+    else:
         raise Exception("Don't know how to flatten type {}".format(type(value)))
-    return vs.flatten(value), vs.unflatten
